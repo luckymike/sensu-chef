42bf14f3a00563ab5b8877fe69aa099477c007d5
Michael F. Weinberg <michael@luckymike.com>
2016-03-02 18:19:34 -0800

    Spot check the files and directories created via sensu LWRPs.

--- test/cookbooks/sensu-test/recipes/default.rb
+++ test/cookbooks/sensu-test/recipes/default.rb
@@ -26,6 +26,7 @@ include_recipe "sensu::default"
 sensu_client node.name do
   address node["ipaddress"]
   subscriptions ["all"]
+  keepalive "warning" => 60, "critical" => 90
 end
 
 sensu_check "test" do
--- test/integration/sysv/serverspec/sysv_spec.rb
+++ test/integration/sysv/serverspec/sysv_spec.rb
@@ -15,3 +15,25 @@ describe service("sensu-api") do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe file("/etc/sensu/conf.d") do
+  it { should be_directory }
+end
+
+describe file("/etc/sensu/conf.d/checks") do
+  it { should be_directory }
+end
+
+describe file("/etc/sensu/conf.d/client.json") do
+  it { should be_file }
+  it { should be_grouped_into "sensu" }
+  its(:content) { should match /"name": / }
+  its(:content) { should match /"warning": 60/ }
+end
+
+describe file("/etc/sensu/conf.d/checks/test.json") do
+  it { should be_file }
+  it { should be_grouped_into "sensu" }
+  its(:content) { should match /"command": "true"/ }
+  its(:content) { should match /"interval": 10/ }
+end
